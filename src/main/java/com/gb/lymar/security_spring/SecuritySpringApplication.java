feee374ad06d0845501578a64e71aa8192720cc6
vlymar90 <7ua.lymar2014@gmail.com>
2021-06-02 20:02:33 +0300

    Initial commit

---
+++ src/main/java/com/gb/lymar/security_spring/SecuritySpringApplication.java
@@ -0,0 +1,13 @@
+package com.gb.lymar.security_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuritySpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecuritySpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gb/lymar/security_spring/SecuritySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gb.lymar.security_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritySpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
